123b1ef0a77d4ecc7794f9ce1996ef041650dfc7
Aabiyah Zehra <81069007+aabiyah@users.noreply.github.com>
2024-12-17 06:42:53 +0000

    Integer to Roman (using Greedy Approach, Hash Map)

--- Problems/Strings/IntegerToRoman.py
+++ Problems/Strings/IntegerToRoman.py
@@ -1,30 +1,29 @@
-class Solution:
-  def intToRoman(self, num: int) -> str:
-      # Define the Roman numeral symbols and their corresponding values
-      roman_map = [
-          ("M", 1000), 
-          ("CM", 900), 
-          ("D", 500), 
-          ("CD", 400), 
-          ("C", 100), 
-          ("XC", 90), 
-          ("L", 50), 
-          ("XL", 40), 
-          ("X", 10), 
-          ("IX", 9), 
-          ("V", 5), 
-          ("IV", 4), 
-          ("I", 1)
-      ]
+class Solution(object):
+    def intToRoman(self, num):
+        roman_map = [
+            ("M", 1000), 
+            ("CM", 900), 
+            ("D", 500), 
+            ("CD", 400), 
+            ("C", 100), 
+            ("XC", 90), 
+            ("L", 50), 
+            ("XL", 40), 
+            ("X", 10), 
+            ("IX", 9), 
+            ("V", 5), 
+            ("IV", 4), 
+            ("I", 1)
+        ]
 
-      # Initialize the result as an empty string
-      result = ""
+        # Initialize the result as an empty string
+        result = ""
 
-      # Iterate through the list of Roman numeral symbols
-      for symbol, value in roman_map:
-          # While the current number is greater than or equal to the Roman value
-          while num >= value:
-              result += symbol  # Append the Roman symbol to the result
-              num -= value  # Subtract the value from the number
+        # Iterate through the list of Roman numeral symbols
+        for symbol, value in roman_map:
+            # While the current number is greater than or equal to the Roman value
+            while num >= value:
+                result += symbol  # Append the Roman symbol to the result
+                num -= value  # Subtract the value from the number
 
-      return result
+        return result
