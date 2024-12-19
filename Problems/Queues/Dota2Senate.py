9334171e236282ff0c3062807853befb421c1070
Aabiyah Zehra <81069007+aabiyah@users.noreply.github.com>
2024-12-19 08:41:53 +0000

    Dota2 Senate

---
+++ Problems/Queues/Dota2Senate.py
@@ -0,0 +1,48 @@
+# In the world of Dota2, there are two parties: the Radiant and the Dire.
+
+# The Dota2 senate consists of senators coming from two parties. Now the Senate wants to decide on a change in the Dota2 game. The voting for this change is a round-based procedure. In each round, each senator can exercise one of the two rights:
+
+# Ban one senator's right: A senator can make another senator lose all his rights in this and all the following rounds.
+# Announce the victory: If this senator found the senators who still have rights to vote are all from the same party, he can announce the victory and decide on the change in the game.
+# Given a string senate representing each senator's party belonging. The character 'R' and 'D' represent the Radiant party and the Dire party. Then if there are n senators, the size of the given string will be n.
+
+# The round-based procedure starts from the first senator to the last senator in the given order. This procedure will last until the end of voting. All the senators who have lost their rights will be skipped during the procedure.
+
+# Suppose every senator is smart enough and will play the best strategy for his own party. Predict which party will finally announce the victory and change the Dota2 game. The output should be "Radiant" or "Dire".
+
+class Solution(object):
+  def predictPartyVictory(self, senate):
+      """
+      :type senate: str
+      :rtype: str
+      """
+      # Queues to store the indices of 'R' (Radiant) and 'D' (Dire) senators
+      radiant = deque()
+      dire = deque()
+
+      # Populate the queues with the indices of Radiant and Dire senators
+      for i, s in enumerate(senate):
+          if s == 'R':
+              radiant.append(i)
+          else:
+              dire.append(i)
+
+      # Total number of senators
+      n = len(senate)
+
+      # Simulate the rounds
+      while radiant and dire:
+          # Compare the indices of the first Radiant and Dire senator
+          r = radiant.popleft()
+          d = dire.popleft()
+
+          # The senator with the smaller index gets to ban the other
+          if r < d:
+              # Radiant bans Dire; add Radiant back to the queue for the next round
+              radiant.append(r + n)
+          else:
+              # Dire bans Radiant; add Dire back to the queue for the next round
+              dire.append(d + n)
+
+      # If Radiant queue is not empty, Radiant wins; otherwise, Dire wins
+      return "Radiant" if radiant else "Dire"
