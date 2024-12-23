e5bf0f919b937639dc4c28169048bf727e7a4c71
Aabiyah Zehra <81069007+aabiyah@users.noreply.github.com>
2024-12-23 11:06:39 +0000

    Isomorphic Strings

---
+++ Problems/Hash Tables and Hash Maps/IsomorphicStrings.py
@@ -0,0 +1,30 @@
+# Given two strings s and t, determine if they are isomorphic.
+
+# Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+# All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+class Solution(object):
+  def isIsomorphic(self, s, t):
+      if len(s) != len(t):
+          return False
+
+      s_to_t = {}
+      t_to_s = {}
+
+      for char_s, char_t in zip(s, t):
+          # Check the mapping from s to t
+          if char_s in s_to_t:
+              if s_to_t[char_s] != char_t:
+                  return False
+          else:
+              s_to_t[char_s] = char_t
+
+          # Check the reverse mapping from t to s
+          if char_t in t_to_s:
+              if t_to_s[char_t] != char_s:
+                  return False
+          else:
+              t_to_s[char_t] = char_s
+
+      return True
